059c883cb6193bbddb4899b57b43ec156a536d2c
kravchuk-avk <kravchuk-avk@gmail.com>
2023-10-11 18:50:33 +0200

    form

---
+++ sendmail.php
@@ -0,0 +1,47 @@
+<?php
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'phpmailer/src/Exception.php';
+	require 'phpmailer/src/PHPMailer.php';
+
+	$mail = new PHPMailer(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->setLanguage('de', 'phpmailer/language/');
+	$mail->IsHTML(true);
+
+	//От кого письмо
+	$mail->setFrom('post@web-studio.app', 'WEB-STUDIO');
+	//Кому отправить
+	$mail->addAddress('kravchuk.avk@gmail.com');
+	//Тема письма
+	$mail->Subject = 'Привет! Есть хороший заказ!';
+
+	//Тело письма
+	$body = '<h1>Встречайте супер письмо!</h1>';
+	
+	if(trim(!empty($_POST['name']))){
+		$body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+	}
+	if(trim(!empty($_POST['email']))){
+		$body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
+	}
+
+	if(trim(!empty($_POST['message']))){
+		$body.='<p><strong>Сообщение:</strong> '.$_POST['message'].'</p>';
+	}
+
+	$mail->Body = $body;
+
+	//Отправляем
+	if (!$mail->send()) {
+		$message = 'Ошибка';
+	} else {
+		$message = 'Данные отправлены!';
+	}
+
+	$response = ['message' => $message];
+
+	header('Content-type: application/json');
+	echo json_encode($response);
+?>
